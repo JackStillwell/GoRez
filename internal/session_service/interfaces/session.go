982c9cbf73a9516c73172d37b9afb1bfd794ee4e
Jack Stillwell <jack.stillwell@outlook.com>
2021-11-08 17:24:23 -0500

    getting ready to test session service

--- internal/session_service/interfaces/session.go
+++ internal/session_service/interfaces/session.go
@@ -2,6 +2,7 @@ package interfaces
 
 import m "github.com/JackStillwell/GoRez/internal/session_service/models"
 
+//go:generate mockgen --source=session.go --destination=../mocks/mock_session.go --package=mock
 type SessionService interface {
 	ReserveSession(int, chan *m.Session)
 	ReleaseSession([]*m.Session)
---
+++ internal/session_service/service_test.go
@@ -0,0 +1,12 @@
+package session_service_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/JackStillwell/GoRez/internal/session_service"
+)
+
+var _ = Describe("Service", func() {
+
+})
